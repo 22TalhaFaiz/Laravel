f00a5b6ba82237fd39369c9e260672f1d521d6ce
22TalhaFaiz <138751622+22TalhaFaiz@users.noreply.github.com>
2024-02-03 00:45:10 +0500

    cc

--- newproject/resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layout')
-@section('mylayout')
-
-<h1>Welcome </h1>
-<a href="{{ route('create') }}">Insert Category</a>
-<a href="{{ route('showcat') }}">Show Category</a>
-
-
-@endsection
